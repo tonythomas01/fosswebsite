aeebb5be81b889e68143ff225112b6a5cbd36ba0
AvaniLodaya <avani9330@gmail.com>
2014-03-29 23:31:03 +0530

    app created, user_info model created

---
+++ register/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+GENDER_CHOICES = (('M', 'Male'),('F','Female'))
+ROLE_CHOICES = (('S','Student'),('M','Mentor'),('B','Both'))
+GOAL_CHOICES = (('startup','startup'),('higher_studies','Higher Studies'),('job','Job'),('other','Others'))
+
+class User_info(models.Model):
+	username = models.CharField(max_length=20, primary_key=True, unique=True, blank=False)
+	password = models.CharField(max_length=20, blank=False)
+	firstname = models.CharField(max_length=20, blank=False, unique=False)
+	lastname = models.CharField(max_lenght=20, blank=False, unique=False)
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+	role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+	email = models.EmailField() 
+	contact = models.CharField(max_lenth=11)
+	interest = models.CharField(max_length=200)
+	achievement = models.CharField(max_length=200)
+	expertise = models.CharField(max_length=200)
+	goal =	models.CharField(max_length=15, choices=GOAL_CHOICES)
