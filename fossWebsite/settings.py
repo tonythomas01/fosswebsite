cf1d66249564a8e398c4d9e0d57414afc9a7cc7b
AvaniLodaya <avani9330@gmail.com>
2014-04-09 20:36:34 +0530

    models to image app added

--- fossWebsite/settings.py
+++ fossWebsite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'achievement',
     'document',
     'apply',	
+    'images',
 )
 
 MIDDLEWARE_CLASSES = (
--- images/models.py
+++ images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+IMAGE_CHOICE = (('achievement','Achievement'),('contest','Contest'),('conference','Conference'),('outing','Outing'),('profile','Profile Pic'),('other','Other'))
+class Image(models.Model):
+	img_id = models.IntegerField(max_length=100, primary_key=True, blank=False, unique=True)
+	img_name = models.CharField(max_length=50, blank=False, null=False)
+	img_description = models.CharField(max_length=200)
+	img_type = models.CharField(max_length=15, choices=IMAGE_CHOICE, blank=False, null=False)
+	img = models.ImageField(upload_to='uploads/photo')
+
+
