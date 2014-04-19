fa770f69c465fabd3ae37af0e3d882ddc0d20ae3
AvaniLodaya <avani9330@gmail.com>
2014-04-19 12:55:04 +0530

    password field removed from profile

--- register/forms.py
+++ register/forms.py
@@ -14,6 +14,7 @@ class NewRegisterForm(ModelForm):
 class ProfileForm(ModelForm):
 	class Meta:
 		model = User_info
+		exclue = ['password']
 class OtherProfileForm(ModelForm):
 	class Meta:
 		model = User_info
