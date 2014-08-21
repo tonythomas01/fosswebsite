1b31bde899c13b188fc1110f624d1bc74541e3e1
Bithin Alangot <bithin2007@gmail.com>
2014-08-21 20:47:44 +0530

    Testing

--- project/models.py
+++ project/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from register.models import User_info
-# Create your models here.
+# Create your models
 STATUS_CHOICES=(('o','Ongoing'),('c','Completed'))
 
 class Project(models.Model):
