fa52f123ff2db188395d686a6633a2c9020ef316
AvaniLodaya <avani9330@gmail.com>
2014-09-06 22:03:34 +0530

    Cleaned views in achievement app

--- achievement/views.py
+++ achievement/views.py
@@ -1,11 +1,17 @@
+# Django libraries
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.template import RequestContext
-from achievement.models import *
-# Create your views here.
 
+# Application specific functions
+from achievement.models import *
+from fossWebsite.helper import error_key, csrf_failure, logged_in
+
+# Create your views here.
 def achieve_viewall(request):
-	is_loggedin = False
-        username = ''
+	"""
+	View to display recent 5 achievements
+	"""
+	username = ''
 	contrib_list = []
 	article_list = []
 	gsoc_list = []
@@ -13,15 +19,13 @@ def achieve_viewall(request):
 	intern_list = []
 	contest_participant_list = []
 	icpc_participant_list = []
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
-        contrib_list = Contribution.objects.all()[:5]
+	if logged_in(request):
+		username = request.session['username']
+	contrib_list = Contribution.objects.all()[:5]
 	article_list = Article.objects.all()[:5]
-        gsoc_list = Gsoc.objects.all()[:5]
-        speaker_list = Speaker.objects.all()[:5]
-        intern_list = Intern.objects.all()[:5]
+	gsoc_list = Gsoc.objects.all()[:5]
+	speaker_list = Speaker.objects.all()[:5]
+	intern_list = Intern.objects.all()[:5]
 	contest_list = Contest_won.objects.all()[:5]
 	if contest_list:	
 		contest_participant_list = []
@@ -36,82 +40,129 @@ def achieve_viewall(request):
 			i_p_objs = ACM_ICPC_Participant.objects.filter(team_name = team)
 			icpc_participant_list.extend(i_p_objs)
 		
-	return render_to_response('achievement/achievement_viewall.html',{'username':username, 'is_loggedin':is_loggedin, 'contrib_list':contrib_list, 'article_list':article_list, 'gsoc_list':gsoc_list, 'speaker_list':speaker_list,'intern_list':intern_list, 'contest_list':contest_list, 'contest_participant_list':contest_participant_list, 'icpc_list':icpc_list, 'icpc_participant_list':icpc_participant_list},RequestContext(request))
+	return render_to_response('achievement/achievement_viewall.html',\
+		{'username':username, 'is_loggedin':logged_in(request), \
+		'contrib_list':contrib_list, 'article_list':article_list, \
+		'gsoc_list':gsoc_list, 'speaker_list':speaker_list, \
+		'intern_list':intern_list, 'contest_list':contest_list, \
+		'contest_participant_list':contest_participant_list, \
+		'icpc_list':icpc_list, 'icpc_participant_list':icpc_participant_list} \
+		,RequestContext(request))
 
 def contrib_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all contributions
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if logged_in(request):
+		username = request.session['username']
 	contrib_list = Contribution.objects.all()
 	if contrib_list:
-		return render_to_response('achievement/contrib_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'contrib_list':contrib_list}, RequestContext(request))
+		return render_to_response('achievement/contrib_viewall.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'contrib_list':contrib_list}, \
+			RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'Contribution'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'Contribution'}, \
+			RequestContext(request))
 
 def article_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all articles published
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if logged_in(request):
+		username = request.session['username']
 	article_list = Article.objects.all()
 	if article_list:
-		return render_to_response('achievement/article_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'article_list':article_list}, RequestContext(request))
+		return render_to_response('achievement/article_viewall.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'article_list':article_list}, \
+			RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'Article'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'Article'}, \
+			RequestContext(request))
 
 
 def gsoc_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all GSOCers
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if logged_in(request):
+		username = request.session['username']
 	gsoc_list = Gsoc.objects.all()
 	if gsoc_list:
-		return render_to_response('achievement/gsoc_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'gsoc_list':gsoc_list}, RequestContext(request))
+		return render_to_response('achievement/gsoc_viewall.html', \
+			{'is_loggedin':loggedin(request), \
+			'username':username, \
+			'gsoc_list':gsoc_list}, \
+			RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'Gsoc'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'Gsoc'}, \
+			RequestContext(request))
 
 
 def speaker_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all Speakers
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if logged_in(request):
+		username = request.session['username']
 	speaker_list = Speaker.objects.all()
 	if speaker_list:
-		return render_to_response('achievement/speaker_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'speaker_list':speaker_list}, RequestContext(request))
+		return render_to_response('achievement/speaker_viewall.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'speaker_list':speaker_list}, \
+			RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'Speaker'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'Speaker'}, \
+			RequestContext(request))
 
 def intern_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all internships done
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if logged_in(request):
+		username = request.session['username']
 	intern_list = Intern.objects.all()
 	if intern_list:
-		return render_to_response('achievement/intern_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'intern_list':intern_list}, RequestContext(request))
+		return render_to_response('achiievement/intern_viewall.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'intern_list':intern_list}, \
+			RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'Internship'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'Internship'}, \
+			RequestContext(request))
 
 def contest_won_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all Contest Won
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if logged_in(request):
+		username = request.session['username']
 	contest_list = Contest_won.objects.all()
 	if contest_list:	
 		contest_participant_list = []
@@ -119,17 +170,26 @@ def contest_won_viewall(request):
 			c_id = contest_won_obj.contest_id
 			c_p_objs = Contest_won_participant.objects.filter(contest_id = c_id)
 			contest_participant_list.extend(c_p_objs)
-		return render_to_response('achievement/contest_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'contest_list':contest_list, 'contest_participant_list':contest_participant_list}, RequestContext(request))
+		return render_to_response('achievement/contest_viewall.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'contest_list':contest_list, \
+			'contest_participant_list':contest_participant_list}, \
+			RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'Contest\'s won'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'Contest\'s won'}, \
+			RequestContext(request))
 
 def icpc_viewall(request):
-	is_loggedin = False
+	"""
+	View to display all ICPCers.
+	"""
 	username = ''
-        if 'is_loggedin' in request.session:
-                if request.session['is_loggedin']:
-                        is_loggedin = True
-                        username = request.session['username']
+	if 'is_loggedin' in request.session:
+		username = request.session['username']
 	icpc_list = ACM_ICPC_detail.objects.all().order_by('yr_of_participation')
 	if icpc_list:
 		icpc_participant_list = []
@@ -137,8 +197,17 @@ def icpc_viewall(request):
 			team = icpc_obj.team_name
 			i_p_objs = ACM_ICPC_Participant.objects.filter(team_name = team)
 			icpc_participant_list.extend(i_p_objs)
-		return render_to_response('achievement/icpc_viewall.html',{'is_loggedin':is_loggedin, 'username':username, 'icpc_list':icpc_list, 'icpc_participant_list':icpc_participant_list}, RequestContext(request))
+		return render_to_response('achievement/icpc_viewall.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'icpc_list':icpc_list, \
+			'icpc_participant_list':icpc_participant_list},\
+			 RequestContext(request))
 	else:
-		return render_to_response('achievement/noview.html',{'is_loggedin':is_loggedin, 'username':username, 'type': 'ACM ICPC Contest'}, RequestContext(request))
+		return render_to_response('achievement/noview.html', \
+			{'is_loggedin':logged_in(request), \
+			'username':username, \
+			'type': 'ACM ICPC Contest'}, \
+			RequestContext(request))
 
 
